90693f55783d4a34ed8e71c641b2c46cae8ef234
T-nishimura0206 <taichi.nishimura0206@gmail.com>
2023-06-20 13:44:21 +0900

    sassファイルのURLを追加しました。

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
-    @vite(['resources/sass/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
